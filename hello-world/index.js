7c7d4be586e249a757795f576cf15649826115ff
pha-ran <pha-ran@naver.com>
2021-02-07 21:22:29 +0900

    dynamic

--- hello-world/index.js
+++ hello-world/index.js
@@ -6,7 +6,16 @@ var app = express(); //express를 실행하여 app object를 초기화
 //   res.send('Hello World!'); // "Hello World!"를 보내기
 // });
 
+app.set('view engine', 'ejs'); //ejs 사용을 위한 express의 view engine에 ejs를 set
 app.use(express.static(__dirname + '/public')); //http 메소드나 루트에 상관없이 요청이 올떄마다 함수실행
+
+app.get('/hello', function(req, res) { //query를 통해 이름 받기 / 모든 query들은 req.query에 저장
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req, res) { //route param을 통해 이름 받기
+  res.render('hello', {name:req.params.nameParam}); //ejs 파일 사용은 res.resder함수 사용
+});
 
 var port = 2021; // 사용할 포트 번호를 port 변수에 대입
 
