6315e10aa8d3bb0ff5591ee24dd66f1fd6d79db9
pha-ran <pha-ran@naver.com>
2021-02-10 12:26:25 +0900

    add [Show, Edit, Update, Destroy]

--- contack-book/index.js
+++ contack-book/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 //DB 설정
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method')); //method의 query값으로 HTTP method를 변경
 
 var contactSchema = mongoose.Schema({
   name:{type:String, require:true, unique:true}, //require 필수입력, unique 중복불가
@@ -33,25 +35,62 @@ var contactSchema = mongoose.Schema({
 
 var Contact = mongoose.model('contact', contactSchema);
 
+//Routes
 app.get('/', function(req, res){
   res.redirect('/contacts');
 });
 
+//Index
 app.get('/contacts', function(req, res){
   Contact.find({}, function(err, contacts){
     if(err) return res.json(err);
     res.render('contacts/index', {contacts:contacts});
+                      //위치, 오브젝트{key:"DB 오브젝트"}
   });
 });
 
+//new
 app.get('/contacts/new', function(req, res){
   res.render('contacts/new');
 });
 
+//create
 app.post('/contacts', function(req, res){
   Contact.create(req.body, function(err, contact){
     if(err) return res.json(err);
     res.redirect('/contacts');
+  });
+});
+
+//show
+app.get('/contacts/:id', function(req, res){
+  Contact.findOne({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/show', {contacts:contacts});
+  });
+});
+
+//edit
+app.get('/contacts/:id/edit', function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/edit', {contacts:contacts});
+  });
+});
+
+//update
+app.put('/contacts/:id', function(req, res){
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts/'+req.params.id);
+  });
+});
+
+//destroy
+app.delete('/contacts/:id', function(req, res){
+  Contact.deleteOne({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts/'+req.params.id);
   });
 });
 
